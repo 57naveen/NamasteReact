380e981b59f4b095a50abef3011b92c0a610695a
57naveen <naveenkishore65@gmail.com>
2025-02-06 14:24:38 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1",{},"Hello from React") //create the element 
+
+const root = ReactDOM.createRoot(document.getElementById("root")) //create the root
+
+root.render(heading); //render the heading into the root
