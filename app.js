51ad3a13944c1c0639ffefbe59a12b3b281824b2
57naveen <naveenkishore65@gmail.com>
2025-02-07 17:37:27 +0530

    ignatiting food app

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
-const heading = React.createElement("h1",{},"Hello from React") //create the element 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root")) //create the root
 
-root.render(heading); //render the heading into the root
+
+/*
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+const Header = () =>
+{
+  return (
+
+    <div className="header">
+    <div className="logo-container">
+        <img className="logo" src="https://marketplace.canva.com/EAFMme8qKdk/1/0/1600w/canva-modern-restaurant-bar-and-grill-food-logo-qG0JgcwrqHQ.jpg"/>
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+    </div>
+  )
+}
+
+const AppLayout =() =>
+{
+  return <div className="app">
+      <Header />
+  </div>
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
+
