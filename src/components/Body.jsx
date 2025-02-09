61cd5c38ae53bdcf2c11616e0db913418e11322c
57naveen <naveenkishore65@gmail.com>
2025-02-09 22:42:35 +0530

    live api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
  
   const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+
+
+  useEffect(()=>
+  {
+      fetchData();
+  },[])
+
+  const fetchData = async () =>
+  {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4790939&lng=78.3690711&collection=83639&tags=layout_CCS_Biryani&sortBy=&filters=&type=rcv2&offset=0&page_type=null")
+
+      const json = await data.json();
+      
+      console.log(json)
+
+      console.log(json.data.cards.slice(3).map(card => card.card.card.info));
+
+      setListOfRestaurants(json.data.cards.slice(3).map(card => card.card.card));
+  }
+
+ 
 
     return (
       <div className="body">  
